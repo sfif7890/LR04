3c75e213b263f4b1ec60cdbd35d616e7d2ac0f7e
sfif7890 <dvornikov.sashakpc1882020@gmail.com>
2022-02-01 21:28:02 +0400

    dev

---
+++ main.py
@@ -0,0 +1,23 @@
+import os
+
+d = {}
+def a():
+    pass
+
+def b():
+    pass
+
+def c():
+    pass
+
+def d():
+    pass
+
+#точка ввода
+print('/T\\')
+print('|.|  Привет я точка входа, напиши "start" чтобы начать')
+while True:
+    vhod = input()
+    if vhod =="start":
+        break
+a()
