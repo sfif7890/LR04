4c0654bd0d4a547a57321def1935225b9f3a88fa
sfif7890 <dvornikov.sashakpc1882020@gmail.com>
2022-02-01 21:45:57 +0400

    c

--- main.py
+++ main.py
@@ -3,12 +3,27 @@ import os
 d = {}
 def a():
     pass
-
 def b():
     pass
 
 def c():
-    pass
+    m = []
+    m1 = []
+    for k in d.keys():
+        m.append(os.path.basename(k))
+    for v in d.values():
+        m1.append(v)
+
+    doubles = {}
+
+    for i in range(len(m)):
+        count = 0
+        for j in range(len(m)):
+            if m[i]==m[j]:
+                if m1[i]==m1[j]:
+                    count+=1
+                doubles[m[i]]=count
+    return d(doubles)
 
 def d():
     pass
